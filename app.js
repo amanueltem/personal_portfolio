074642f5999679bbc1367c59e43d0a6bbb5f5a46
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-07-07 23:14:23 +0300

    more on styling

---
+++ app.js
@@ -0,0 +1,8 @@
+const sections=document.querySelectorAll(".section");
+const secBtns=document.querySelectorAll(".controlls");
+const secBtn=document.querySelectorAll(".control");
+const allSections=document.querySelectorAll(".main-content")
+function pageTranstions(){
+//button click active class
+
+}
